1aea7c5df3367eb22b7b34351eddab07bd31dae8
softdevelopp <softdevelopp@gmail.com>
2022-09-14 21:30:22 -0500

    firts commit bancoPoo

---
+++ Models/Banco.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bancoPoo.Models
+{
+    class Banco
+    {
+        public string NumeroCuenta { get; set; }
+        public string Identifica { get; set; }
+        public string nombre { get; set; }
+        public string dirección { get; set; }
+        public int saldo { get; set; }
+
+
+
+        public Banco(string cuenta, string identidad, string nombre, string direccion, int saldo)
+        {
+            this.NumeroCuenta = cuenta;
+            this.Identifica = identidad;
+            this.nombre = nombre;
+            this.dirección = direccion;
+            this.saldo = saldo;
+
+
+
+
+        }
+
+        public void Depositar(int cantidad)
+        {
+            this.saldo += cantidad;
+
+        }
+
+        public void retirar(int retiro)
+        {
+            this.saldo-= retiro;
+        }
+
+        public int consultaSaldo()
+        {
+            return saldo;
+        }
+
+
+
+    }   
+
+   
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//Iimplementacion de un cliente en el banco
+using bancoPoo.Models;
+
+Banco cliente = new Banco("123", "12023", "Carlos Moreno", "Pte de Cabi", 200000);
+Console.WriteLine("saldo Inicial del cliente : "+cliente.saldo);
+//realizare un deposito
+cliente.Depositar(70000);
+
+Console.WriteLine("saldo una vez depositado : "+cliente.saldo);
+
+//realizare un retiro
+cliente.retirar(30000);
+
+Console.WriteLine("saldo una vez retirado : "+cliente.saldo);
+
+
+//cosultare la cuenta
+Console.WriteLine("saldo final una vez depositado y retirado : "+cliente.consultaSaldo());
